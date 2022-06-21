08e0cd1fb1eaf9f29562050ea37575a056c66c86
nick thomas <nichoth@gmail.com>
2022-06-21 11:01:47 -0700

    add cloudinary as store

---
+++ cloudinary.js
@@ -0,0 +1,23 @@
+let cloudinary = require("cloudinary").v2;
+const { getHash } = require('@nichoth/multihash')
+
+module.exports = function createCloudinary (config) {
+    cloudinary.config(config)
+
+    return {
+        write: function (file, hash) {
+            hash = hash || getHash(file)
+            var slugifiedHash = encodeURIComponent('' + hash)
+
+            return new Promise(function (resolve, reject) {
+                cloudinary.uploader.upload(file, {
+                    public_id: slugifiedHash,
+                    overwrite: true
+                }, function (err, res) {
+                    if (err) return reject(err)
+                    resolve({ hash: slugifiedHash, response: res })
+                })
+            })
+        }
+    }
+}
